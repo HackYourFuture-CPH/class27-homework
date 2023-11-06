9acab685d111ce333e8bb9cc441e7db744557817
katayouny <k.yousefzadeh@gmail.com>
2023-11-06 13:17:34 +0100

    excercise

--- 3_javascript/javascript1/week1/javascript_class_excercises/javascript.js
+++ 3_javascript/javascript1/week1/javascript_class_excercises/javascript.js
@@ -1,1 +1,15 @@
-console.log("Hello from javascript page2);
+console.log('Hello from javascript page2');
+const name = 'katayoun';
+let age = 38;
+console.log(name + ' ' + age);
+console.log(name[0]);
+console.log(name[name.length - 1]);
+
+let a;
+a = 24 * 55;
+console.log(a);
+
+let MyArray = ['Alpha', 'Beta', 'Gama', 10, 20, 30, true, false, true];
+console.log(MyArray[3]);
+
+
