83fc2120af748598c41c4a17e35cff4b467d138e
katayouny <k.yousefzadeh@gmail.com>
2023-11-07 18:09:59 +0100

    adding different javascript codes

---
+++ 3_javascript/javascript1/week1/katayoun_javascript_classexcercises/javascript.js
@@ -0,0 +1,48 @@
+console.log('Hello from javascript.js page');
+const name = 'katayoun';
+let age = 38;
+console.log(name + ' ' + age);
+console.log(name[0]);
+console.log(name[name.length - 1]);
+
+let a;
+a = 24 * 55;
+console.log(a);
+
+let MyArray = ['Alpha', 'Beta', 'Gama', 10, 20, 30, true, false, true];
+console.log(MyArray[3]);
+
+console.log(typeof '0');
+
+
+
+console.log('.....................');
+
+console.log(1 == 1);//true
+console.log(1 == 2);//fals
+
+console.log(1 != 2);//true
+
+console.log('HELLO' == 2);//fals
+
+console.log('1' == 1);//true
+// both value and type should be true
+console.log('1' !== 1);//true
+console.log('--------------------------------------');
+
+console.log(1 > 8);//fals
+
+console.log(1 === 1 && 3 === 4);//fals
+console.log((1 !== 1 || 3 === 4) && true);//fals
+
+console.log("hello " + 23);
+console.log("hello " + "23");
+
+if (1 === 2) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+
+
