718e5b297df93f7dc04ef5b55e1b572270bc459c
Niloufar <nilooooofar97@gmail.com>
2023-11-07 14:08:56 +0100

    dog age homework finished

---
+++ 3_javascript/javascript1/week1/dog-age/dogAge.js
@@ -0,0 +1,12 @@
+let dogYearOfBirth = 2020;
+let dogYearFuture = 2030;
+let dogHumanYear = dogYearFuture - dogYearOfBirth;
+let dogYear = dogHumanYear * 7;
+let shouldShowResultInDogYears = true;
+
+if (shouldShowResultInDogYears){
+    console.log("Your dog will be " + dogYear + " dog years old in " + dogYearFuture )
+}else{
+    console.log("Your dog will be " + dogHumanYear + " human years old in " + dogYearFuture);
+  
+}
