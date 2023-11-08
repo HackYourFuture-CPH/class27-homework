600182a682b17d7b930eab615864de879f2f3b75
Youssef Topaji <youssefftopaji@gmail.com>
2023-11-08 20:53:41 +0100

    Dog age calculator

---
+++ 3_javascript/javascript1/week1/JS-Smart-ease/goodboy oldboy/dog-age.js
@@ -0,0 +1,10 @@
+const dogYearOfBirth= 2020;
+const dogYearFuture= 2027;
+
+dogYear = (dogYearFuture - dogYearOfBirth)*7;
+
+if (shouldShowResultInDogYears = true){
+    console.log("Your dog will be " + dogYear+ " human years old in " + dogYearFuture);
+} else {
+    console.log("Your dog will be " + (dogYearFuture-dogYearOfBirth) + " dogs years old in " + dogYearFuture);
+}
