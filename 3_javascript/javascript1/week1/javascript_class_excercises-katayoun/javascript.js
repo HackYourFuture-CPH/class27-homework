0e347f9d519a878e05c8dbc844c4d7ae772179d0
katayouny <k.yousefzadeh@gmail.com>
2023-11-08 19:46:43 +0100

    Javascript codes

---
+++ 3_javascript/javascript1/week1/javascript_class_excercises-katayoun/javascript.js
@@ -0,0 +1,31 @@
+console.log('Hello from javascript page2');
+const name = 'katayoun';
+let age = 38;
+console.log(name + ' ' + age);
+console.log(name[0]);
+console.log(name[name.length - 1]);
+
+let a;
+a = 24 * 55;
+console.log(a);
+
+let MyList = ['Alpha', 'Beta', 'Gama', 10, 20, 30, true, false, true];
+console.log(MyList[3]);
+
+let list = [
+    "1",
+    "2",
+    "3",
+    "3"
+];
+console.log(list);
+
+
+let human_Array = {
+    name: 'katy',
+    family: 'Yousefzade'
+};
+console.log(human_Array);
+
+
+
