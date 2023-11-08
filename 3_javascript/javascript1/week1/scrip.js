3833390d9a09f4542bd78db0cd4263e666befee6
Hadis Jamali <hadisjamali95@gmail.com>
2023-11-08 09:55:49 +0100

    update RP

--- 3_javascript/javascript1/week1/scrip.js
+++ 3_javascript/javascript1/week1/scrip.js
@@ -27,3 +27,60 @@ if (shouldShowResultInDogYears === true) {
 }
 
 //Housey pricey (A house price estimator)
+
+const volumeInMeters1 = 8 * 10 * 10;
+const housePrice1 = 2500000;
+const gardenSizeInM2Peter = 100;
+const calculatedHousePrice = volumeInMeters1 * 2.5 * 1000 + gardenSizeInM2Peter * 300;
+if (housePrice1 > calculatedHousePrice) {
+  console.log("too expensive");
+} else if (housePrice1 === calculatedHousePrice) {
+  console.log("price is good");
+} else {
+  console.log("cheap");
+}
+
+const volumeInMeters2 = 5 * 11 * 8;
+const housePrice2 = 1000000;
+const gardenSizeInM2Julia = 70;
+const calculatedHousePrice2 = volumeInMeters2 * 2.5 * 1000 + gardenSizeInM2Julia * 300;
+if (housePrice2 > calculatedHousePrice2) {
+  console.log("too expensive");
+} else if (housePrice2 === calculatedHousePrice2) {
+  console.log("price is good");
+} else {
+  console.log("cheap");
+}
+
+//Ez Namey (Startup name generator) Optional
+
+const firstWords = [
+  "Easy",
+  "Awesome",
+  "Corporate",
+  "Nice",
+  "Great",
+  "Big",
+  "Team",
+  "Prety",
+  "Beautiful",
+  "Brilliant",
+];
+const secondWords = [
+  "Hope",
+  "Vestas",
+  "Dansk",
+  "Fotex",
+  "Rema",
+  "Jysk",
+  "Ikea",
+  "Lidl",
+  "Aldi",
+  "Asus",
+];
+
+const randomNumber = Math.floor(Math.random() * 10);
+console.log(randomNumber);
+const startupName = firstWords[randomNumber] + " " + secondWords[randomNumber];
+console.log(startupName);
+console.log(startupName.length);
