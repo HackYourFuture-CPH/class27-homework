3520a8072dd12fc178afa19d04e8d76ce7cbd560
Gizem Savci <129002582+GizemSavci@users.noreply.github.com>
2023-11-09 23:37:11 +0100

    Implemente task 3

--- 3_javascript/javascript1/week1/main.js
+++ 3_javascript/javascript1/week1/main.js
@@ -19,3 +19,23 @@ if (shouldShowResultInDogYears === true){
     console.log("Your dog will be " + dogAge +" human years old in " + dogYearFuture)
 }
 
+//Housey Pricey
+
+function  calculator(wide, deep, high, gardenSizeInM2, price){
+    const volumeInMeters = wide*deep*high;
+    const housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2 * 300;
+    //return housePrice;
+    if (price < housePrice){
+        console.log("The house is cheaper than market value.")
+    }else if (price > housePrice){
+        console.log("The house is more expensive than market value.")
+    }else{
+        console.log("The price is equal to market value.")
+    }
+}
+
+//function for Peter. Peter pays more than market value. :(
+calculator(8, 10, 10, 100, 2500000)
+
+//function for Julia. Julia pays less than market value. ^.^
+calculator(5, 11, 8, 70, 1000000)
