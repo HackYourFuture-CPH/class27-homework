923ffc5e4a57ad551dd5e6e9f2afe089923cbdb0
Gizem Savci <129002582+GizemSavci@users.noreply.github.com>
2023-11-09 23:05:08 +0100

    Implemented task 2

--- 3_javascript/javascript1/week1/main.js
+++ 3_javascript/javascript1/week1/main.js
@@ -4,3 +4,18 @@ let yearFuture = 2045;
 let age = yearFuture - yearOfBirth;
 
 console.log("You will be " + age + " years old in " + yearFuture)
+
+
+//Goodboy-Oldboy
+let dogYearOfBirth = 2023;
+let dogYearFuture = 2027;
+let dogYear = 7;
+let shouldShowResultInDogYears = true;
+let dogAge = dogYearFuture - dogYearOfBirth;
+
+if (shouldShowResultInDogYears === true){
+    console.log("Your dog will be " + dogAge*dogYear + " dog years old in " + dogYearFuture)
+}else{
+    console.log("Your dog will be " + dogAge +" human years old in " + dogYearFuture)
+}
+
