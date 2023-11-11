73b6e75ba9822a56706728e33a0729254e057a72
katayouny <k.yousefzadeh@gmail.com>
2023-11-11 16:39:24 +0100

    Editing excercise 2 with suggestions

--- 3_javascript/javascript1/week1/javascript.js
+++ 3_javascript/javascript1/week1/javascript.js
@@ -1,3 +1,4 @@
+// exc. 1 ---------------------------------------------------------------------------------
 console.log('------------- Age - ify(A future age calculator)-------------');
 
 let yearOfBirth = 1976;
@@ -7,32 +8,39 @@ let age = yearFuture - yearOfBirth;
 console.log(`You will be ${age} years old in ${yearFuture}`);
 console.log('\n');
 
+// eXC. 2 -----------------------------------------------------------------------------------
 
 console.log('----------- Goodboy-Oldboy (A dog age calculator) -----------');
 let dogYearOfBirth = 2018;
 let dogYearFuture = 2036;
 let dogYear;
 
-shouldShowResultInDogYears: Boolean;
+let shouldShowResultInDogYears;
 
-let choice = prompt("Do you wnat to know your dog age based on Dog years or Human years calculation? \n Type the word dog or human please");
+let choice = prompt(`Do you wnat to know your dog's age based on dog years or human years calculation? \n 
+Type the word "dog" or "human" please`);
 
-if (choice == 'dog') {
-    let shouldShowResultInDogYears = (choice === 'true');
-    console.log('You have chosen the word ' + choice);
-    console.log('Your dog age calculation based on dog years: ');
+if (choice === "dog") {
+    shouldShowResultInDogYears = true;
+    console.log(`You have chosen the word "${choice}".`);
     dogYear = (yearFuture - dogYearOfBirth) * 7;
-    console.log('Your dog will be ' + dogYear + ' dog years old in ' + dogYearFuture);
-} else if (choice == 'human') {
-    console.log('You have chosen the word ' + choice);
-    console.log('Your dog age calculation based on human years: ');
+    console.log(`Your dog will be ${dogYear} dog years old in ${dogYearFuture}`);
+
+} else if (choice === "human") {
+    shouldShowResultInDogYears = false;
+    console.log(`You have chosen the word "${choice}".`);
     dogYear = yearFuture - dogYearOfBirth;
-    console.log('Your dog will be ' + dogYear + ' human years old in ' + dogYearFuture);
+    console.log(`Your dog will be ${dogYear} human years old in ${dogYearFuture}`);
+
 } else {
-    console.log('You have not entered dog neither human. \n Please run the page again and enter one of them to perform the calculations');
+    alert("You have not entered dog neither human. \n
+     Please run the page again and enter one of them to perform the calculations");
+
+  return;
 }
 console.log('\n');
 
+// eXC. 3 -----------------------------------------------------------------------------------
 
 console.log('----------- Housey pricey (A house price estimator) -----------');
 
