191b289735443364f049616f7e844e25d8f14561
Ihor Kulytskyi <pionerikk25@gmail.com>
2023-11-11 22:08:07 +0100

    Changes after comments

--- 3_javascript/javascript1/week1/Startup-name.js
+++ 3_javascript/javascript1/week1/Startup-name.js
@@ -3,5 +3,5 @@ const firstWords = ["Easy", "Awesome", "Corporate", "Super", "Good"];
         const randomNumber = Math.floor(Math.random() * 5);     
         let startupName = firstWords[randomNumber] + " " + secondWords[randomNumber];
         console.log(startupName);
-        let length = startupName.length
+        let length = startupName.length - 1;
         console.log("The startup: " + startupName + " contains " + length + " characters.");
