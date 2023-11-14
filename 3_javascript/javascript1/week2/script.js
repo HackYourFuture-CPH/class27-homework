ab64565257834bcf19bdb0c4a47a7ca8ce54c9fd
Niloufar <nilooooofar97@gmail.com>
2023-11-14 09:12:49 +0100

    change console.log to return in functions

--- 3_javascript/javascript1/week2/script.js
+++ 3_javascript/javascript1/week2/script.js
@@ -1,4 +1,4 @@
- const getFullname = (
+const getFullname = (
   firstname,
   surname,
   useFormalName = false,
@@ -8,11 +8,11 @@
     (firstname !== "" && surname !== "" && firstname.trim() !== "",
     surname.trim() !== "")
   ) {
-     (useFormalName && areYouMale) 
-     ? console.log("lord " + firstname + " " + surname)
-     : (useFormalName && !areYouMale)
-     ? console.log("lady " + firstname + " " + surname) 
-     :console.log(firstname + " " + surname);  
+    useFormalName && areYouMale
+      ? console.log("lord " + firstname + " " + surname)
+      : useFormalName && !areYouMale
+      ? console.log("lady " + firstname + " " + surname)
+      : console.log(firstname + " " + surname);
   } else {
     console.log("provide a valid value for firstname and surname");
   }
@@ -36,26 +36,26 @@ const getEventDay = (daysUntilEvent) => {
   const today = new Date();
   const dayIndex = today.getDay();
   const eventDayIndex = (dayIndex + daysUntilEvent) % 7;
-  console.log(`The event will be held on a ${weekdays[eventDayIndex]}`);
+  return weekdays[eventDayIndex];
 };
-getEventDay(9);
-getEventDay(5);
+console.log(`The event will be held on a ${getEventDay(9)}`);
+console.log(`The event will be held on a ${getEventDay(5)}`);
 
 // weather wear
 const clothesToWear = (temperature) => {
   if (temperature >= 20) {
-    console.log("It is very hot. shorts and T-shirt is a good choise");
+    return ("It is very hot. shorts and T-shirt is a good choise");
   } else if (temperature >= 10) {
-    console.log("It is warm. jeans and shirt is good");
+    return ("It is warm. jeans and shirt is good");
   } else if (temperature >= 5) {
-    console.log("It is cool .wear jeans and long sleeve");
+    return ("It is cool .wear jeans and long sleeve");
   } else {
-    console.log("It is cold. Make sure to have jacket with you");
+    return ("It is cold. Make sure to have jacket with you");
   }
 };
-clothesToWear(15);
-clothesToWear(2);
-clothesToWear(25);
+console.log(clothesToWear(15));
+console.log(clothesToWear(2));
+console.log(clothesToWear(25));
 
 // Student Manager
 const class07Students = [];
@@ -74,7 +74,7 @@ function addStudentToClass(studentName) {
 }
 
 function getNumberOfStudents() {
-  console.log(class07Students.length);
+  return (class07Students.length);
 }
 addStudentToClass("Niloufar");
 addStudentToClass("Armin");
@@ -87,7 +87,7 @@ addStudentToClass("Mino");
 addStudentToClass("Hassan");
 addStudentToClass("Queen");
 console.log(class07Students);
-getNumberOfStudents();
+console.log(getNumberOfStudents());
 
 // candy helper
 let boughtCandyPrices = [];
