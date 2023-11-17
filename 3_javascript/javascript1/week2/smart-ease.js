dab9154baca0c96a456b247fe5a3ae29bcb48d86
Viki <victoriadem.317@gmail.com>
2023-11-17 11:48:24 +0100

    Weather wear, Student manager - exercises 3&4

--- 3_javascript/javascript1/week2/smart-ease.js
+++ 3_javascript/javascript1/week2/smart-ease.js
@@ -22,7 +22,6 @@ console.log(fullname1);
 console.log(fullname2); 
 console.log(fullname3);
 
-
 //Event application
 
 const weekdays = [
@@ -56,3 +55,46 @@ console.log(myOutfitForToday);
 const myOutfitForTodayTwo = (whatDoIHaveToWear(10));
 console.log(myOutfitForTodayTwo);
 
+//Student manager
+
+const class07Students = [];
+function addStudentToClass(studentName) {
+    for (let i = 0; i < class07Students.length; i++) {
+        if (class07Students[i] === studentName) {
+          console.log("Student " + studentName + " is already in the class");
+          return;}
+    }
+    if (class07Students.length <= 6 || studentName === "Queen") {
+        class07Students.push(studentName);
+        console.log("Dear " + studentName + ", there is always place for you in the class")
+      } else if (studentName === "") {
+        console.log("Please, enter your name")
+      } else {
+        console.log("Cannot add more students to class 07");
+      } 
+}
+addStudentToClass("Emil");
+addStudentToClass("Kate");
+addStudentToClass("Anne");
+addStudentToClass("Nick");
+addStudentToClass("Sonya");
+addStudentToClass("Peter");
+addStudentToClass("Queen");
+addStudentToClass("Lisa");
+
+console.log(class07Students);
+
+
+function getNumberOfStudents() {
+  console.log(class07Students.length);
+}
+getNumberOfStudents(); 
+
+// I tried to add value of class07Students.length max, 
+//but it didn't work, and I can't figure out where to add it so it works, 
+//and I'm not sure that it is useful also there - 
+//class07Students.length = Math.max(7) 
+
+//I also don't understand why I can't make "Queen" a special object in function, 
+//and that requirement is applying to all other names..
+//I need some advice so can improve it.  
