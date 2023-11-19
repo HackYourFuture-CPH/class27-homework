eab4aea956cab43fd4c948b130eb949f40c874e0
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2023-11-19 12:36:12 +0100

    Update week2.js

--- 3_javascript/javascript1/week2/week2.js
+++ 3_javascript/javascript1/week2/week2.js
@@ -23,9 +23,7 @@ function getEventWeekday(eventy) {
     
     const numberDayNow = dateNow.getDay();
  
-    const addDay=  eventy % 7;
- 
-    const nextDate = numberDayNow + addDay;
+    const nextDate = (numberDayNow + eventy) % 7;
     console.log('event on', weekday[nextDate]);
 
 }
@@ -92,7 +90,6 @@ function addStudentToClass(studentName) {
 
 function getNumberOfStudents() {
     return class07Students.length;
-    console.log(`The number of students is ${class07Students.length}`)
   // You write code here
 }
 
@@ -107,4 +104,45 @@ addStudentToClass("");
 addStudentToClass("N6");
 addStudentToClass("N7");
 console.log(class07Students);
+console.log(`The number of students is ${class07Students.length}`);
 
+
+
+//  Candy helper optional
+let boughtCandyPrices = [];
+const typeCandy = ["sweet", "chocolate", "toffee", "chewing-gum"];
+const priceCandy = [0.5, 0.7, 1.1, 0.03];
+const amountToSpend = Math.random() * 100;
+
+console.log("amountToSpend", amountToSpend);
+
+function addCandy(candyType, weight) {
+    let price = 0;
+    for (i = 0; i < typeCandy.length; i++) {
+        if (typeCandy[i] === candyType) {
+            price = priceCandy[i] * weight;
+        }
+    }
+    boughtCandyPrices.push(price);
+
+}
+
+addCandy("sweet", 20);
+addCandy("chocolate", 20);
+
+function canBuyMoreCandy(amountToSpend, boughtCandyPrices) {
+    let totalSum = 0;
+    let i = 0;
+    
+    while (i < boughtCandyPrices.length) {
+        totalSum =+ boughtCandyPrices[i];
+        if (totalSum < amountToSpend) {
+            console.log("You can buy more, so please do!");
+        } else {
+            console.log("Enough candy for you!");
+        }
+        i++;
+    }
+}
+
+canBuyMoreCandy(amountToSpend, boughtCandyPrices);
