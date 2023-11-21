e43d6c801b557120ef07f3cddb8f1e8d86167eb9
Viki <victoriadem.317@gmail.com>
2023-11-21 18:03:04 +0100

    Finished homework javascript1-week1 (changes after suggestions)

--- 3_javascript/javascript1/week1/smart-ease.js
+++ 3_javascript/javascript1/week1/smart-ease.js
@@ -1,29 +1,27 @@
 console.log("Javascript/week1");
 //Age-ify (A future age calculator)
 const yearOfBirth = 1992;
-let yearFuture = 2024;
+const yearFuture = 2024;
 const age = yearFuture - yearOfBirth;
 console.log(age);
 console.log("You will be " + age + " years old in " + yearFuture);
 
 //Goodboy-Oldboy (A dog age calculator)
 const dogYearOfBirth = 2017;
-let dogYearFuture = 2027;
-let dogYear = dogYearFuture - dogYearOfBirth;
+const dogYearFuture = 2027;
+const dogYear = dogYearFuture - dogYearOfBirth;
 console.log(dogYear);
-let dogYearsCompareToHuman = dogYear * 7;
+const dogYearsCompareToHuman = dogYear * 7;
 console.log(dogYearsCompareToHuman);
-shouldShowResultInDogYears = true;
-shouldShowResultInDogYears = false;
-function test(shouldShowResultInDogYears) {
+function dogsAge(shouldShowResultInDogYears) {
     if (shouldShowResultInDogYears) {
         return console.log("Your dog will be " + dogYear + " human years old in " + dogYearFuture);
     }
         return console.log("Your dog will be " + dogYearsCompareToHuman + " dog years old in " + dogYearFuture); 
 }
 
-test(true);
-test(false);
+dogsAge(true);
+dogsAge(false);
 
 //Housey pricey (A house price estimator)
 
@@ -33,11 +31,15 @@ let houseWideForPeter = 8;
 let houseDeepForPeter = 10;
 let houseHighForPeter =10;
 let houseGardenSizeForPeter = 100;
-const volumeInMetersForPeter = 8*10*10*2.5*1000;
-console.log(volumeInMetersForPeter);
-const realHousePriceForPeter = volumeInMetersForPeter+(houseGardenSizeForPeter*300);
+const volumeInMetersForPeter = houseWideForPeter*houseDeepForPeter*houseHighForPeter;
+const realHousePriceForPeter = volumeInMetersForPeter * 2.5 * 1000 +(houseGardenSizeForPeter*300);
 console.log(realHousePriceForPeter);
-console.log("The real price for Peter during calculation is " + realHousePriceForPeter + ", and it means that he is paying much more " + "(" + housePriceForPeter + ")" + " for the house with those parameters.");
+if (housePriceForPeter > realHousePriceForPeter) {
+    console.log("The house is too expensive.")
+    }
+    else {
+    console.log("Good deal.")
+    }
 
 //House for Julia
 const housePriceForJulia = 1000000;
@@ -45,15 +47,18 @@ let houseWideForJulia = 5;
 let houseDeepForJulia = 11;
 let houseHighForJulia = 8;
 let houseGardenSizeForJulia = 70;
-const volumeInMetersForJulia = 5*11*8*2.5*1000;
-console.log(volumeInMetersForJulia);
-const realHousePriceForJulia = volumeInMetersForJulia+(houseGardenSizeForJulia*300);
+const volumeInMetersForJulia = houseWideForJulia*houseDeepForJulia*houseHighForJulia;
+const realHousePriceForJulia = volumeInMetersForJulia * 2.5 * 1000 +(houseGardenSizeForJulia*300);
 console.log(realHousePriceForJulia);
-console.log("The real price for Julia during calculation is " + realHousePriceForJulia + ", and it means that she is paying much less " + "(" + housePriceForJulia + ")" + " for the house with those parameters.");
-
+if (housePriceForJulia > realHousePriceForJulia) {
+    console.log("The house is too expensive.")
+    }
+    else {
+    console.log("Good deal.")
+}
 
 //Ez Namey (Startup name generator)
-let firstWords = [
+const firstWord = [
     "Funny",
     "Ambitious",
     "Intelligent",
@@ -65,7 +70,7 @@ let firstWords = [
     "Attractive",
     "Pro-active"
 ];
-let secondWords = [
+const secondWord = [
     "Donkey",
     "Wagon",
     "Cow",
@@ -78,5 +83,5 @@ let secondWords = [
     "Bees"
 ];
 const randomNumber = Math.floor(Math.random() * 10);
-const startupName = firstWords[randomNumber] + " " + secondWords[randomNumber];
-console.log("The startup: " + startupName + " contains " + startupName.length + " characters");
+const startupName = firstWord[randomNumber] + " " + secondWord[randomNumber];
+console.log("The startup: " + startupName + " contains " + (startupName.length-1) + " characters");
