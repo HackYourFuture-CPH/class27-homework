eb18b936dd0ad126a5e202c0d86eff0b4d612277
Gizem Savci <129002582+GizemSavci@users.noreply.github.com>
2023-11-26 12:10:12 +0100

    Implemented Task 2 Time Calculator

--- 3_javascript/javascript1/week3/index.js
+++ 3_javascript/javascript1/week3/index.js
@@ -16,3 +16,14 @@ const names = [
   // Code done
   
   console.log(names);
+
+  //When will we be there??
+
+function timeCalculator(speed, destinationDistance){
+    const  timeHour = Math.floor((destinationDistance / speed)%60);
+    const timeMin = Math.round((destinationDistance % speed) / speed * 60);
+    console.log(timeHour + '  hours and '+ timeMin + ' minutes.');
+}
+
+console.log(timeCalculator(50, 432));
+console.log(timeCalculator(60, 98));
