0ddd8c0f3a7456e47ceb4673fdd45bde39b02611
Gizem Savci <129002582+GizemSavci@users.noreply.github.com>
2023-11-26 12:04:04 +0100

    Implemented Task 1 Array Removal

---
+++ 3_javascript/javascript1/week3/index.js
@@ -0,0 +1,18 @@
+//Item array removal
+const names = [
+    "Peter",
+    "Ahmad",
+    "Yana",
+    "kristina",
+    "Rasmus",
+    "Samuel",
+    "katrine",
+    "Tala",
+  ];
+  const nameToRemove = "Ahmad";
+  
+  // Write some code here
+  names.splice(1,1);
+  // Code done
+  
+  console.log(names);
