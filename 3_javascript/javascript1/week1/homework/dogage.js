7e13f416611ac5cc8c3c2f42a52622e35dfce3c7
Monika_M <m.marciniak89@gmail.com>
2023-12-02 18:35:07 +0100

    I changed the dog age file

--- 3_javascript/javascript1/week1/homework/dogage.js
+++ 3_javascript/javascript1/week1/homework/dogage.js
@@ -6,8 +6,9 @@ const dogYear = dogYearFuture - dogYearOfBirth;
 
 if (shouldShowResultInDogYears) {
   console.log(
-    "Your dog will be " + dogYear * 15 + " dogs years old in 2027"
+    "Your dog will be " + `${dogYear}`  * 15 + " dogs years old in 2027"
   );
 } else {
-  console.log("Your dog will be 70 human years old in 2027");
+  console.log("Your dog will be " `${dogYear}` + "human years old in 2027");
 }
+console.log(dogYear);
