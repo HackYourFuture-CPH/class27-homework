1e141d056f1ae7a69d318e2a0da72447e7244b31
Monika_M <m.marciniak89@gmail.com>
2023-12-02 19:30:00 +0100

    I added js files

--- 3_javascript/javascript1/week2/homework2ndweek/fullname.js
+++ 3_javascript/javascript1/week2/homework2ndweek/fullname.js
@@ -1,15 +1,16 @@
 const firstname1 = getFullName("Monika", "Marciniak");
 const firstname2 = getFullName("John", "Smith");
 
-function getFullName (firstName, surName, isWoman = true)  {
+function getFullName (firstName, surName, isWoman = true, useFormalName)  {
    if  (typeof firstName !== "string" || typeof surName !== "string")
    return "Provide first name and last name"
-  if ( isWoman) {
-    return "Lady " + firstName + " " + surName;
+  if (isWoman) {
+    return "Lady " + `${firstName}` + " " + `${surName}`;
   } else {
-    return "Lord " + firstName + " " + surName;
+    return "Lord " + `${firstName}` + " " + `${surName}`;
   }
 }
 
 console.log(firstname1);
 console.log(firstname2);
+
