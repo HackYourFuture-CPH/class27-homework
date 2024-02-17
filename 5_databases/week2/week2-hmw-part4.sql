f99c3684c5610c868498c15b30cda9136f492184
Gizem Savci <129002582+GizemSavci@users.noreply.github.com>
2024-02-17 14:27:42 +0100

    Drop Client_Type & Registration_Number_Type

--- 5_databases/week2/week2-hmw-part4.sql
+++ 5_databases/week2/week2-hmw-part4.sql
@@ -4,17 +4,54 @@ CREATE DATABASE Investment_Bank
 
 USE Investment_Bank;
 
+-- Client_Type does not exist anymore
+-- SELECT * FROM Client_Type;
 
-SELECT * FROM Client_Type;
-SELECT * FROM Registration_Number_Type;
+-- Registration_Number_Type does not exist anymore
+-- SELECT * FROM Registration_Number_Type;
+
 SELECT * FROM Instrument_Type;
-
+SELECT * FROM Client;
+SELECT * FROM Complex_Instruments;
+SELECT * FROM Non_Complex_Instruments;
 
 -- Create Client Type 
 CREATE TABLE Client_Type (
     id INT AUTO_INCREMENT PRIMARY KEY,
     client_type VARCHAR(255) NOT NULL
 );
+
+-- ------------------------------
+-- To Get ClientTypeID Constraint
+SHOW CREATE TABLE Client
+
+-- Drop FOREIGN KEY Constraint
+ALTER TABLE Client
+DROP FOREIGN KEY Client_ibfk_1;
+
+-- Drop ClientTypeID Column
+ALTER TABLE Client
+DROP COLUMN ClientTypeID;
+
+-- Delete Client_Type
+DROP TABLE Client_Type;
+-- ------------------------------
+
+-- To Get RegNumKey Constraint
+SHOW CREATE Table Client;
+
+-- Drop FOREIGN KEY Constraint
+ALTER TABLE Client
+DROP FOREIGN KEY Client_ibfk_2;
+
+-- Drop RegNumKey Column
+ALTER TABLE Client
+DROP COLUMN RegNumKey;
+
+-- Delete Registration_Number_Type
+DROP TABLE Registration_Number_Type;
+-- ------------------------------
+
 
 -- Create Table Registration Number Type
 CREATE TABLE Registration_Number_Type (
@@ -57,20 +94,37 @@ CREATE TABLE Non_Complex_Instruments (
     FOREIGN KEY (id) REFERENCES Instrument_Type (id)
 );
 
+ALTER TABLE Client
+ADD client_type VARCHAR(255); 
+
+ALTER TABLE Client
+ADD registration_number_type VARCHAR(3);
+
 
 -- Insert Client Types. Retrieved From Jyske Bank Investment Documents
-INSERT INTO Client_Type (client_type) values ('Retail clients');
-INSERT INTO Client_Type (client_type) values ('Professional clients');
-INSERT INTO Client_Type (client_type) values ('Eligible counterparties');
+-- INSERT INTO Client_Type (client_type) values ('Retail clients');
+-- INSERT INTO Client_Type (client_type) values ('Professional clients');
+-- INSERT INTO Client_Type (client_type) values ('Eligible counterparties');
 
 -- Insert Registration Number Types
-INSERT INTO Registration_Number_Type (registration_number_type) values ('CPR');
-INSERT INTO Registration_Number_Type (registration_number_type) values ('CVR');
+-- INSERT INTO Registration_Number_Type (registration_number_type) values ('CPR');
+-- INSERT INTO Registration_Number_Type (registration_number_type) values ('CVR');
 
 
 -- Insert Instrument_Type
 INSERT INTO Instrument_Type (instrument_type) VALUES ('Non Complex Instruments');
 INSERT INTO Instrument_Type (instrument_type) VALUES ('Complex Instruments');
 
--- This didn't work
--- INSERT INTO Complex_Instruments (complex_instrument) values ('Index-linked bonds');
+-- Insert Complex Instruments
+INSERT INTO Complex_Instruments (complex_instrument) values ('Index-linked bonds');
+INSERT INTO Complex_Instruments (complex_instrument) values ('Convertible bond');
+INSERT INTO Complex_Instruments (complex_instrument) values ('Government bonds, other');
+INSERT INTO Complex_Instruments (complex_instrument) values ('Structured bond');
+INSERT INTO Complex_Instruments (complex_instrument) values ('Foreign mortgage bond');
+INSERT INTO Complex_Instruments (complex_instrument) values ('Corporate bond, other complex');
+INSERT INTO Complex_Instruments (complex_instrument) values ('Interest rate swap');
+INSERT INTO Complex_Instruments (complex_instrument) values ('Currency swap');
+
+
+
+INSERT INTO Client (name, address, email, phone, Investments, client_type, registration_number_type) values ('Gorm Den Gamle', 'Gormsgade Jelling', 'gorm@gmail.com', '1234567', 2, 'Eligible counterparties', 'CPR');
