1528fd2fb5111cfea359655bd57a54fd4ed8260f
Hadis Jamali <hadisjamali95@gmail.com>
2024-02-19 22:44:25 +0100

    add minimist

--- 6_nodejs/week1/avg.js
+++ 6_nodejs/week1/avg.js
@@ -1,3 +1,8 @@
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+//process
+
 const args = process.argv;
 
 function average(num1, num2, num3) {
@@ -5,3 +10,20 @@ function average(num1, num2, num3) {
   console.log(avg);
 }
 average(Number(args[2]), Number(args[3]), Number(args[4]));
+
+//minimist
+
+const minimist = require("minimist");
+
+const minimistArgs = minimist(process.argv.slice(2));
+const values = Object.keys(minimistArgs).filter((key) => key !== "_")
+.map((key) => Number(minimistArgs[key]));
+
+let sum = 0;
+for (let i = 0; i < values.length; i++) {
+  sum += values[i];
+}
+const minimistAverage = sum / values.length;
+
+console.log("Input values:", values);
+console.log("Average:", minimistAverage);
