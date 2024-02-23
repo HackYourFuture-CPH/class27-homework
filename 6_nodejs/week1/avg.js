91d70c8fd4178838e4dcda342182a02a8753f8ca
sanaz <sanazre123@gmail.com>
2024-02-23 09:38:03 +0100

    ver0

---
+++ 6_nodejs/week1/avg.js
@@ -0,0 +1,8 @@
+let avg = 0;
+for (let j = 2; j < process.argv.length; j++) {
+  const a = parseInt(process.argv[2]);
+  const b = parseInt(process.argv[3]);
+  const c = parseInt(process.argv[4]);
+  avg = (a + b + c) / 3;
+}
+console.log(avg);
