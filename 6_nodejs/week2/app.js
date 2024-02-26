ad21b959956c1c88926e7c4479cff31abf603d1a
Niloufar <nilooooofar97@gmail.com>
2024-02-26 09:21:43 +0100

    GET /documents/:id task

--- 6_nodejs/week2/app.js
+++ 6_nodejs/week2/app.js
@@ -1,31 +1,38 @@
-const express = require("express");
-const fs = require("fs");
-const data = JSON.parse(fs.readFileSync("document.json"));
+import express from "express";
+import fs from "fs";
 
-const app = express();
 const port = process.env.PORT || 3000;
-
-// Support parsing JSON requests
-app.use(express.json());
+const data = JSON.parse(fs.readFileSync("./document.json"));
+const app = express();
 
 app.get("/", (req, res) => {
   res.send("This is a search engine");
 });
-
+// GET /search
 app.get("/search", (req, res) => {
-    const query = req.query.q;
-    if (!query) {
-      return res.status(200).json({ data: data, message: "ok" });
-    } else {
-      const filteredData = data.filter((item) =>
-        Object.values(item).some((value) => typeof value === "string" && value.includes(query))
+  const query = req.query.q;
+  if (query) {
+    const filteredData = data.filter((item) => {
+      return Object.values(item).some(
+        (value) => typeof value === "string" && value.includes(query)
       );
-      if (filteredData.length === 0)
-        return res.status(404).json({ data: null, message: "item not found" });
-      return res.status(200).json({ data: filteredData, message: "ok" });
+    });
+    if (filteredData.length === 0) {
+      return res.send("not found");
     }
-  });
-
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+    return res.json({ data: filteredData, message: "ok" });
+  }
+  res.json({ data: data, message: "ok" });
 });
+
+// GET /documents/:id
+app.get("/search/:id" , (req, res) => {
+  const dataId = +req.params.id;
+  const myData = data.find(data => data.id === dataId);
+  if(!myData){
+    return res.status(404).json({data: null , message : "not found"});
+  }
+ res.json({data: myData , message : "ok"});
+});
+
+app.listen(port, () => console.log(`listening on port ${port}`));
