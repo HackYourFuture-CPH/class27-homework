41550df2c2478cb944fddf1d58063a647d4e53e8
Niloufar <nilooooofar97@gmail.com>
2024-02-28 09:27:32 +0100

    done

--- 6_nodejs/week2/app.js
+++ 6_nodejs/week2/app.js
@@ -1,9 +1,12 @@
 import express from "express";
+// import {bodyParser} from 'body-parser'
 import fs from "fs";
 
 const port = process.env.PORT || 3000;
 const data = JSON.parse(fs.readFileSync("./document.json"));
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("This is a search engine");
@@ -26,13 +29,48 @@ app.get("/search", (req, res) => {
 });
 
 // GET /documents/:id
-app.get("/search/:id" , (req, res) => {
+app.get("/search/:id", (req, res) => {
   const dataId = +req.params.id;
-  const myData = data.find(data => data.id === dataId);
-  if(!myData){
-    return res.status(404).json({data: null , message : "not found"});
+  const myData = data.find((data) => data.id === dataId);
+  if (!myData) {
+    return res.status(404).json({ data: null, message: "not found" });
   }
- res.json({data: myData , message : "ok"});
+  res.json({ data: myData, message: "ok" });
+});
+
+// POST /search
+app.post("/search", (req, res) => {
+  const query = req.query.q;
+  console.log(req.body.fields);
+  const body = req.body;
+  console.log(body);
+  if (query && body && body.fields !== undefined) {
+    res.status(400).send("can't provide both query and field");
+  }
+  if (query) {
+    const filteredData = data.filter((item) =>
+      Object.values(item).some(
+        (value) => typeof value === "string" && value.includes(query)
+      )
+    );
+    if (filteredData.length === 0) {
+      return res.status(404).json({ data: null, message: "not found" });
+    }
+    return res.json({ data: filteredData, message: "ok" });
+  } else if (body && body.fields) {
+    const keys = Object.keys(body.fields);
+    const dataWithField = data.filter(item => {
+        return keys.every(key => {
+            return item.hasOwnProperty(key);
+        });
+    });
+    if (dataWithField.length === 0) {
+        return res.status(404).json({ data: null, message: "not found" });
+    }
+    return res.status(200).json({ data: dataWithField, message: "ok" });
+} else {
+    return res.json({ data: data, message: "ok" });
+  }
 });
 
 app.listen(port, () => console.log(`listening on port ${port}`));
