d09a603fb05f34e01dc6845da35036a29da6d46b
anjali <anjalikumari74454@gmail.com>
2024-02-29 16:43:36 +0100

    Optional Question

--- 6_nodejs/week1/avg.js
+++ 6_nodejs/week1/avg.js
@@ -8,6 +8,30 @@ function calculateAverage(numbers) {
 }
 
 //retrieves the command line arguments
-const args = process.argv.slice(2);
-const average = calculateAverage(args);
+const arguments = process.argv.slice(2);
+const average = calculateAverage(arguments);
 console.log(average);
+
+//--------Optional question--------------------------------------
+//arguments are mix of numbers and string
+function printNumbersAndStrings(args) {
+  if (args.length === 0) {
+    console.log("There is no arguments.");
+    return;
+  }
+  const numbers = [];
+  const strings = [];
+  for (let i = 0; i < args.length; i++) {
+    const num = parseFloat(args[i]);
+    if (!isNaN(num)) {
+      numbers.push(num);
+    } else {
+      strings.push(args[i]);
+    }
+  }
+  console.log("Numbers:", numbers);
+  console.log("Strings:", strings);
+}
+
+const arguments1 = process.argv.slice(2);
+printNumbersAndStrings(arguments1);
