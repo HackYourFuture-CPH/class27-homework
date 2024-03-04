1874dc101d0fd899534d2f257e483c513a0fb47b
Gizem Savci <129002582+GizemSavci@users.noreply.github.com>
2024-03-04 21:43:02 +0100

    Create app.js

---
+++ 6_nodejs/week2/app.js
@@ -0,0 +1,14 @@
+import express from 'express'
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Support parsing JSON requests
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("This is a search engine");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
