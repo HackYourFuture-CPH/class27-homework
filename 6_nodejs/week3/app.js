40f5fff4006f9d11d61af3540d2d8d0bed780d9b
Hadis Jamali <hadisjamali95@gmail.com>
2024-03-05 13:32:08 +0100

    add new photo

--- 6_nodejs/week3/app.js
+++ 6_nodejs/week3/app.js
@@ -5,7 +5,7 @@ const knexInstance = knex({
     host: process.env.DB_HOST || "127.0.0.1",
     port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "Hm4310913229",
+    password: process.env.DB_PASSWORD || "password",
     database: process.env.DB_NAME || "hyf_node_week3_warmup",
     multipleStatements: true,
   },
