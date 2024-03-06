628ef38dc797f7d1f9a5e6ac4ca827c99e28ea04
anjali <anjalikumari74454@gmail.com>
2024-03-06 13:28:07 +0100

    set-up done

---
+++ 6_nodejs/week2/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("This is a search engine");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
