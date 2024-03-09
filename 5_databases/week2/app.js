7dc466b48bede02149d63f96d2a9fd7af06dae26
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2024-03-09 10:44:24 +0100

    Update app.js

--- 5_databases/week2/app.js
+++ 5_databases/week2/app.js
@@ -1,6 +1,16 @@
 import express from "express";
+import { readFileSync } from "node:fs";
+
 const app = express();
 const port = process.env.PORT || 3003;
+
+
+const data = readFileSync("./documents.json", (err, data) => {
+  if (err) throw err;
+});
+const documents = JSON.parse(data);
+console.log(documents);
+
 
 // Support parsing JSON requests
 app.use(express.json());
@@ -10,8 +20,19 @@ app.get("/", (req, res) => {
 });
 
 app.get("/search", (req, res) => {
-    console.log(req.query);
+  const q = req.query.q;
+  const fields = req.body.fields;
+
+  if (q && fields) {
+    res.status(400).send('Request and fields both can not be provided');
+  } else if (q) {
+      return res.json(documents);
+    } else {
+      const resp = documents.filter(docum => docum.value.includes(q));
+      res.json(resp);
+    }
 })
+
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
