5598d5b42b7d6a6c93db811269f8a3a62ea30928
Gizem Savci <129002582+GizemSavci@users.noreply.github.com>
2024-03-11 19:54:30 +0100

    Implement todolist

--- 7_react/react1/week1/todo-app/src/App.jsx
+++ 7_react/react1/week1/todo-app/src/App.jsx
@@ -1,14 +1,27 @@
-
-
-
-
+import TodoList from "./components/TodoList/TodoList";
 
 function App() {
-
-
+const todos = [
+  {
+    id: 1,
+    task: "Get out of bed",
+    deadline: "Wed Sep 13 2017"
+  },
+  {
+    id: 2,
+    task: "Brush teeth",
+    deadline: "Thu Sep 14 2017"
+  },
+  {
+    id:3,
+    task:"Eat breakfast",
+    deadline:"Fri Sep 15 2017"
+  }];
   return (
-    <p>Hello</p>
+    <div className="App">
+      <TodoList todos={todos} />
+    </div>
   )
 }
 
-export default App
+export default App;
