728eef5fe7aa480946d0057523ffd75e9d9f9bfe
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2024-03-22 23:23:51 +0100

    delete comments

--- 7_react/react1/week1/src/App.js
+++ 7_react/react1/week1/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import AppTodo from './todo_list/TodoList';
 
@@ -7,18 +7,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <AppTodo/>
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
       </header>
     </div>
   );
