ef6316f71fe86655f56b4590bc3c7a8dfafda715
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2024-03-22 23:24:24 +0100

    move todos

--- 7_react/react1/week1/src/todo_list/TodoList.jsx
+++ 7_react/react1/week1/src/todo_list/TodoList.jsx
@@ -1,49 +1,31 @@
 import { TodoItem } from "../todoItem/TodoItem";
-const AppTodo = () => {
-    
-    const todos = [
-      {
-        name: "Get out of bed",
-        deadline: "Wed Sep 13 2017",
-        status: true,
-      },
-      {
-        name: "Brush teeth",
-        deadline: "Thu Sep 14 2017",
-        status: false,
-      },
-      {
-        name: "Eat breakfast",
-        deadline: "Fri Sep 15 2017",
-        status: false,
-      },
-    ];
+import todos from "../todoItem/todos";
 
-   
+function AppTodo () {
 
-    const TodoList = (props) => {
-        console.log(props);
+  const data = [...todos];
+  
+  const TodoList = (props) => {
 
         return (
-
-            <ul>
-                {props.todos.map((todo) => {
-                    return (
-                        <TodoItem
-                            name={todo.name}
-                            deadline={todo.deadline}
-                            status={todo.status}
-                        />
-                    )
-                })};
-            </ul>
+          <ul>
+            {props.todos.map((todo) => {
+              return (
+                <TodoItem
+                  name={todo.name}
+                  deadline={todo.deadline}
+                  status={todo.status}
+                />
+              );
+            })}
+          </ul>
         );
     };
     
     return (
       <div>
-        <h2>My  To-Do List</h2>
-        <TodoList todos={todos} />
+        <h2>My To-Do List</h2>
+        <TodoList todos={data} />
       </div>
     );
 };
