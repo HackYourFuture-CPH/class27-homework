77b1276614af5860f3955b4b1195065dfb0d3e4b
Gizem Savci <129002582+GizemSavci@users.noreply.github.com>
2024-03-26 16:14:08 +0100

    Add conditional rendering

--- 7_react/react1/week2/todo-app/src/components/TodoList/TodoList.jsx
+++ 7_react/react1/week2/todo-app/src/components/TodoList/TodoList.jsx
@@ -4,14 +4,18 @@ function TodoList({todos, removeTodo}) {
     return (
         <div className="todo-list">
             <h2>Todo List of Gizem</h2>
-            {todos.map(todo => (
-                <TodoItem
-                key={todo.id}
-                todo={todo}
-                deadline={todo.deadline}
-                removeTodo={removeTodo}
-                />
-            ))}
+            {todos.length === 0 ? (
+                <p>Nothing to do. You are free as a bird!</p>
+            ):(
+                todos.map(todo => (
+                    <TodoItem
+                    key={todo.id}
+                    todo={todo}
+                    deadline={todo.deadline}
+                    removeTodo={removeTodo}
+                    />
+            ))
+            )}
         </div>
     );
 }
