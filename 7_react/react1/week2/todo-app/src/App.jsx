f66562f1e06aba1d9a9b0740ce49da974f0409dd
Gizem Savci <129002582+GizemSavci@users.noreply.github.com>
2024-03-26 15:34:27 +0100

    Add timer component

--- 7_react/react1/week2/todo-app/src/App.jsx
+++ 7_react/react1/week2/todo-app/src/App.jsx
@@ -1,27 +1,35 @@
 import TodoList from "./components/TodoList/TodoList";
-
-const todos = [
-  {
-    id: 1,
-    task: "Get out of bed",
-    deadline: "Wed Sep 13 2017"
-  },
-  {
-    id: 2,
-    task: "Brush teeth",
-    deadline: "Thu Sep 14 2017"
-  },
-  {
-    id:3,
-    task:"Eat breakfast",
-    deadline:"Fri Sep 15 2017"
-  }];
+import Timer from "./components/Timer/Timer";
+import { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([
+      {
+        id: 1,
+        task: "Get out of bed",
+        deadline: "Wed Sep 13 2017"
+      },
+      {
+        id: 2,
+        task: "Brush teeth",
+        deadline: "Thu Sep 14 2017"
+      },
+      {
+        id:3,
+        task:"Eat breakfast",
+        deadline:"Fri Sep 15 2017"
+      }
+  ]);
+
+  const removeTodo = (todoIdToRemove) => {
+    setTodos(todos.filter(todo => todo.id !== todoIdToRemove));
+  };
 
   return (
     <div className="App">
-      <TodoList todos={todos} />
+      <Timer/>
+      <TodoList todos={todos} removeTodo={removeTodo}/>
+
     </div>
   )
 }
--- 7_react/react1/week2/todo-app/src/components/TodoList/TodoList.jsx
+++ 7_react/react1/week2/todo-app/src/components/TodoList/TodoList.jsx
@@ -1,11 +1,16 @@
 import TodoItem from "../TodoItem/TodoItem";
 
-function TodoList(props) {
+function TodoList({todos, removeTodo}) {
     return (
         <div className="todo-list">
             <h2>Todo List of Gizem</h2>
-            {props.todos.map(todo => (
-                <TodoItem key={todo.id} task={todo.task} deadline={todo.deadline} />
+            {todos.map(todo => (
+                <TodoItem
+                key={todo.id}
+                todo={todo}
+                deadline={todo.deadline}
+                removeTodo={removeTodo}
+                />
             ))}
         </div>
     );
